82b35b1c5aea225f466d522155af12781d4350cd
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-15 08:29:46 -0400

    wip

---
+++ src/client.ts
@@ -0,0 +1,31 @@
+import { RpcClient, RpcSerialization } from "@effect/rpc";
+import { FetchHttpClient } from "@effect/platform";
+import { Console, Effect, Layer, Stream } from "effect";
+import { UserRpcs } from "./rpc";
+
+const ProtocolLive = RpcClient.layerProtocolHttp({
+	url: "http://localhost:8787/rpc",
+}).pipe(
+	Layer.provide([
+		// use fetch for http requests
+		FetchHttpClient.layer,
+		// use ndjson for serialization
+		RpcSerialization.layerNdjson,
+	]),
+);
+
+// Use the client
+const program = Effect.gen(function* () {
+	const client = yield* RpcClient.make(UserRpcs);
+	console.log(yield* client.UserById({ id: "0" }));
+	// yield* client.UserList().pipe(
+	// 	Stream.take(3),
+	// 	Stream.runForEach((user) => Console.log(user)),
+	// );
+}).pipe(Effect.scoped);
+
+program.pipe(
+	Effect.provide(ProtocolLive),
+	Effect.catchAllCause(Effect.logError),
+	Effect.runPromise,
+);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { Effect, Layer, Logger, LogLevel, ManagedRuntime, Schema } from "effect"
-import { Rpc, RpcClient, RpcGroup, RpcSerialization, RpcServer } from "@effect/rpc"
-import { HttpServer } from "@effect/platform"
+import { ReadableStream } from "@cloudflare/workers-types";
+import { Console, Effect, Layer, ManagedRuntime, Stream } from "effect";
+import {
+	Rpc,
+	RpcClient,
+	RpcClientError,
+	RpcGroup,
+	RpcMessage,
+	RpcSerialization,
+	RpcServer,
+} from "@effect/rpc";
+import { HttpApp, Headers } from "@effect/platform";
 import { DurableObject } from "cloudflare:workers";
-
-const MyRpcs = RpcGroup.make(
-	Rpc.make("Echo", {
-		success: Schema.Struct({ text: Schema.String }),
-		payload: { text: Schema.String }
-	})
-)
+import { User, UserRpcs } from "./rpc";
+import { constVoid } from "effect/Function";
 
 /**
  * Welcome to Cloudflare Workers! This is your first Durable Objects application.
@@ -25,7 +29,7 @@ const MyRpcs = RpcGroup.make(
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
 export class MyDurableObject extends DurableObject<Env> {
-	readonly runtime: ManagedRuntime.ManagedRuntime<never, never>
+	readonly runtime: ManagedRuntime.ManagedRuntime<never, never>;
 
 	/**
 	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
@@ -36,7 +40,7 @@ export class MyDurableObject extends DurableObject<Env> {
 	 */
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
-		this.runtime = ManagedRuntime.make(Layer.empty)
+		this.runtime = ManagedRuntime.make(Layer.empty);
 	}
 
 	/**
@@ -50,24 +54,48 @@ export class MyDurableObject extends DurableObject<Env> {
 		return `Hello, ${name}!`;
 	}
 
-	async rpc(): Promise<Uint8Array | ReadableStream> {
-		return Promise.resolve(new Uint8Array())
+	async rpc(
+		payload: string | Uint8Array,
+	): Promise<Uint8Array | ReadableStream> {
+		return Effect.gen(function* () {
+			const handlersContext = yield* UserRpcs.toLayer({
+				UserById: ({ id }) => Effect.succeed(new User({ id, name: "Max" })),
+				UserCreate: ({ name }) => Effect.succeed(new User({ id: "0", name })),
+				UserList: () =>
+					Stream.forever(
+						Stream.make(new User({ id: "0", name: "Max" })).pipe(
+							Stream.tap(() => Effect.sleep("100 millis")),
+						),
+					),
+			}).pipe(Layer.build);
+
+			const parser = RpcSerialization.ndjson.unsafeMake();
+
+			const requests = parser.decode(
+				payload,
+			) as Array<RpcMessage.FromClientEncoded>;
+			const request = requests[0];
+
+			if (request._tag === "Request") {
+				const handler = yield* UserRpcs.accessHandler(request.tag as any).pipe(
+					Effect.provide(handlersContext),
+				);
+
+				const response = handler(
+					request.payload as any,
+					Headers.fromInput(request.headers),
+				);
+				if (Effect.isEffect(response)) {
+					return yield* Effect.map(response, (user) => {
+						return parser.encode(user);
+					});
+				}
+			}
+
+			return yield* Effect.succeed(new Uint8Array());
+		}).pipe(Effect.scoped, Effect.runPromise);
 	}
 }
-
-const MyRpcsLayer = MyRpcs.toLayer({
-	Echo: ({ text }) => Effect.succeed({ text })
-})
-
-const { handler } = RpcServer.toWebHandler(MyRpcs, {
-	layer: Layer.mergeAll(
-		MyRpcsLayer,
-		RpcSerialization.layerJson,
-		HttpServer.layerContext
-	).pipe(
-		Layer.provideMerge(Logger.minimumLogLevel(LogLevel.All)),
-	),
-})
 
 export default {
 	/**
@@ -79,25 +107,108 @@ export default {
 	 * @returns The response to be sent back to the client
 	 */
 	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url)
+		const url = new URL(request.url);
 
 		if (request.method === "POST" && url.pathname === "/rpc") {
-			// Create a stub to open a communication channel with the Durable Object
-			// instance named "foo".
-			//
-			// Requests from all Workers to the Durable Object instance named "foo"
-			// will go to a single remote Durable Object instance.
 			const stub = env.MY_DURABLE_OBJECT.getByName("foo");
 
-			// Call the `sayHello()` RPC method on the stub to invoke the method on
-			// the remote Durable Object instance.
-			// const greeting = await stub.sayHello("world");
+			return Effect.gen(function* () {
+				const memoMap = yield* Layer.makeMemoMap;
+				const scope = yield* Effect.scope;
 
-			const response = await handler(request)
-			console.log(response)
+				const rpcClientContext = yield* layerProtocolDurableObject({
+					handleRequest: (payload) => stub.rpc(payload),
+				}).pipe(
+					Layer.provideMerge(RpcSerialization.layerNdjson),
+					Layer.buildWithMemoMap(memoMap, scope),
+				);
+
+				const rpcClient = yield* RpcClient.make(UserRpcs).pipe(
+					Effect.provide(rpcClientContext),
+				);
+
+				const rpcHandlerContext = yield* UserRpcs.toLayer({
+					UserById: (params) => Effect.orDie(rpcClient.UserById(params)),
+					UserCreate: (params) => Effect.orDie(rpcClient.UserCreate(params)),
+					UserList: () => Stream.orDie(rpcClient.UserList()),
+				}).pipe(
+					Layer.provideMerge(RpcServer.layerProtocolHttp({ path: "/rpc" })),
+					Layer.provideMerge(RpcSerialization.layerNdjson),
+					Layer.buildWithMemoMap(memoMap, scope),
+				);
+
+				const handler = yield* RpcServer.toHttpApp(UserRpcs).pipe(
+					Effect.map(HttpApp.toWebHandler),
+					Effect.provide(rpcHandlerContext),
+				);
+
+				return yield* Effect.promise(() => handler(request));
+			}).pipe(
+				Effect.scoped,
+				Effect.tapErrorCause(Effect.logError),
+				Effect.runPromise,
+			);
 		}
 
-		return new Response("Not found", { status: 404 })
+		return new Response("Not found", { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
 
+const makeProtocolDurableObject = (options: {
+	readonly handleRequest: (
+		payload: string | Uint8Array,
+	) => Promise<Uint8Array | ReadableStream>;
+}) =>
+	RpcClient.Protocol.make(
+		Effect.fnUntraced(function* (writeResponse) {
+			const serialization = yield* RpcSerialization.RpcSerialization;
+			const parser = serialization.unsafeMake();
+
+			const send: (
+				request: RpcMessage.FromClientEncoded,
+			) => Effect.Effect<void, RpcClientError.RpcClientError> =
+				Effect.fnUntraced(function* (request) {
+					switch (request._tag) {
+						case "Request": {
+							const payload = parser.encode(request);
+							if (payload === undefined) {
+								return yield* Effect.void;
+							}
+							const result = yield* Effect.promise(() =>
+								options.handleRequest(payload),
+							);
+							if (result instanceof Uint8Array) {
+								const responses = parser.decode(
+									result,
+								) as Array<RpcMessage.FromServerEncoded>;
+								if (responses.length === 0) {
+									return yield* Effect.void;
+								}
+								let i = 0;
+								return yield* Effect.whileLoop({
+									while: () => i < responses.length,
+									body: () => writeResponse(responses[i++]),
+									step: constVoid,
+								});
+							}
+							break;
+						}
+						case "Interrupt": {
+							break;
+						}
+					}
+				});
+
+			return {
+				send,
+				supportsAck: false,
+				supportsTransferables: false,
+			};
+		}),
+	);
+
+const layerProtocolDurableObject = (options: {
+	readonly handleRequest: (
+		payload: string | Uint8Array,
+	) => Promise<Uint8Array | ReadableStream>;
+}) => Layer.effect(RpcClient.Protocol, makeProtocolDurableObject(options));
